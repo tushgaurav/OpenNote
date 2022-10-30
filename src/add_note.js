19c0cf5c08644734b3fddb611dda9fef0593daa1
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2022-10-30 18:35:26 +0530

    fixed note not showing bug

--- src/add_note.js
+++ src/add_note.js
@@ -6,8 +6,8 @@ document.querySelector("form").addEventListener("submit", submitForm);
 function submitForm(e) {
   e.preventDefault();
   const title = document.querySelector("#title").value;
-  const content = document.querySelector("#content").value;
-  console.log(ipcRenderer);
+  const content = document.querySelector("textarea").value;
+
   const note = {
     title: title,
     content: content,
